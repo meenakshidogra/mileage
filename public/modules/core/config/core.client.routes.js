76e485f84a4988c39ad647c7142cb3eea52e2573
meenakshidogra <khushi.rana4u@gmail.com>
2015-06-17 08:23:45 -0700

    Added the routing state for the home page

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -1,16 +1,30 @@
 'use strict';
 
 // Setting up route
-angular.module('core').config(['$stateProvider', '$urlRouterProvider',
-	function($stateProvider, $urlRouterProvider) {
-		// Redirect to home view when route not found
-		$urlRouterProvider.otherwise('/');
+angular.module('core')
+.config(['$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+        // Redirect to home view when route not found
+        $urlRouterProvider.otherwise('/');
 
-		// Home state routing
-		$stateProvider.
-		state('home', {
-			url: '/',
-			templateUrl: 'modules/core/views/home.client.view.html'
-		});
-	}
+        // Home state routing
+        $stateProvider.
+            state('home', {
+            url: '/',
+            views: {
+                '': {
+                    templateUrl: 'modules/core/views/home.client.view.html'
+                },
+                //the child views will be defined here (absolutely named)
+                'cars@home': {
+                    templateUrl: 'modules/cars/views/list-cars.client.view.html'
+                }
+
+                //// another child view
+                //'readings@home': {
+                    //templateUrl: 'modules/core/views/home.client.view.html'
+                //}
+            }
+        });
+    }
 ]);
